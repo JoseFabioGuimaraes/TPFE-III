226cd2e5fedf084c2370aa2ce737021abdcf9854
josefabioguimaraes <jfabioguimaraes@gmail.com>
2024-03-20 21:23:58 -0300

    Aula -3

---
+++ Aula03/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
+import Aluno from './components/Aluno'
+
+const App = () => {
+  return (
+    <div>
+      <EstouConseguindoAprenderReact estouConseguindo={false}/> 
+
+      {
+        [
+        {nome:"Jose", email: "jose@gmail.com", curso: "SI", media: 5}, 
+        {nome:"Maria", email: "maria@gmail.com", curso: "SI",media: 7},
+        {nome:"Joao", email: "joao@gmail.com", curso: "SI",media:8}
+      ].map(
+        (aluno)=> <Aluno nome={aluno.nome} curso={aluno.curso} email={aluno.email} media={aluno.media}/>
+        )
+      }
+
+    </div>
+  )
+}
+
+export default App
