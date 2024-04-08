f603314d8616757374b200f3ba329e53c651a0ca
josefabioguimaraes <jfabioguimaraes@gmail.com>
2024-04-07 22:12:15 -0300

    Aula04

---
+++ Aula04/src/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p> TESTE DE CSS</p>
+        <img src = 'https://img.nsctotal.com.br/wp-content/uploads/2023/06/tres-versoes-de-homem-aranha-800x531.jpeg'/>
+    </div>
+  )
+}
+
+export default Exercicio1
